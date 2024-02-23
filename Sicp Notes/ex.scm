78a7e4f8a3362233657eb10bc30880779ad1834b
Assem <assem2002@gmail.com>
2024-02-24 01:50:20 +0200

    new

--- Sicp Notes/ex.scm
+++ Sicp Notes/ex.scm
@@ -1411,14 +1411,122 @@ ex 2.70
 ------------------------------------------------------------------
 ex 2.71
 
-;for and number 2^x > sum(2^(x-1)+ ......+2^(0)) ---> so the process of building the tree will cause in what's called a degenerate tree which just a linked list so,
-most frequent letter will be 1 bit
-least frequent letter will be (n-1) bits
+;for any number 2^x > sum(2^(x-1)+ ......+2^(0)) ---> so the process of building the tree will cause in what's called a degenerate tree which just a linked list so,
+;most frequent letter will be 1 bit
+;least frequent letter will be (n-1) bits
 
 
 ------------------------------------------------------------------
 ex 2.72
 
-most frequent symbol -> o(n) if you search the unlucky branch first ,o(1) if you get lucky and search in the good branch.
-least frequent symbol -> you can assume that every step of depth makes the list (n) become (n-1) so the result will be (n*(n-1))/2 which is o(n^2).
+;most frequent symbol -> o(n) if you search the unlucky branch first ,o(1) if you get lucky and search in the good branch.
+;least frequent symbol -> you can assume that every step of depth makes the list (n) become (n-1) so the result will be (n*(n-1))/2 which is o(n^2).
 
+---------------------------------------------------------------------
+ex 2.73
+a)
+
+;what happened : we made a whole a table with indeces (operation (which is just deriv operation) , operators), so based on the operation there's gonna be a certain procedure returened to handle this
+; case, though this doesn't much of sense as you have one way of handling the derivation of + or * or whatever.
+
+; why can't we use the predicate number and variable in the table (date directed way)? : the data directed dispatch supposes that the data sent has a tag (type) that corresponds to some procedure in 
+;the genreric interface we made but with numbers there's no tag unless you make it like that "(number 1)" which isn't what expected from the user, and for the variable predicate it also needs a tag;
+; while something like + - * is a tag for specifc type of deriv procedure,we just dispatch it and operate on the data.
+;;;;;;;;;;
+b)
+
+(define addened car)
+(define augend cadr)
+(define multipler car )
+(define multiplicand cadr)
+
+;The followings are just different implemntations in case you want to handle multiple argument in the experssion. 
+
+;(define (multiplicad exp) 
+;  (if (null?(cddr exp)) (cadr exp)  
+;                           (list '* (cdr exp)) ))
+;(define (augend exp) 
+;  (if (null? (cddr exp)) (cadr exp)
+;      (list '+ (cdr exp))))
+
+
+(define (install-deriv-sum) 
+  (define (deriv-sum exp var) ( 
+    (make-sum (deriv (addend exp) var)
+		(deriv (augend exp) var))))
+  (put 'deriv '+ deriv-sum))
+
+
+(define (install-deriv-product)
+  (define (deriv-product exp var) 
+    ((make-sum (make-product
+				(multiplier exp)
+				(deriv (multiplicand exp) var))
+					(make-product
+					(deriv (multiplier exp) var)
+					(multiplicand exp)))))
+  
+  
+  (put 'deriv '* deriv-product ) )
+;;;;;;;;;;;;;;;;;;;;;;;
+  
+  
+c)  
+
+(define base car)
+(define exponent cadr)
+
+(define (make-exponent base expon)
+  (cond ((= expon 0) 1)
+        ((= expon 1) base)
+  		(else (list '** base expon))))
+
+
+(define (install-deriv-exponent)
+(define base car)
+(define exponent cadr)
+
+(define (make-exponent base expon)
+  (cond ((= expon 0) 1)
+        ((= expon 1) base)
+  		(else (list '** base expon))))
+  (define (deriv-exponent exp var)
+     	(make-product 
+      				(make-product 
+      						(exponent exp)
+      						(make-exponent base (- (exponent exp) 1) ))
+                   (deriv (base exp) var)))
+  (put 'deriv '** deriv-exponent))
+  
+  
+;;;;;;;;;;
+d)
+; I don't quite understand what the question implies, but if we switched the indeces of the table we would simply swap the put function arguments.
+----------------------------------------------------------------------------
+
+ex 2.74
+
+;The division must supply an install procedure to place its own functions in the divison system table
+;The file provided should have its first element the name of the division or a code of the divison (ID)
+
+;you can apply your specific apply prodcedure as the apply generic provided here won't work.
+;or you can make the installation based on type of two arguments which honestly doesn't make any sense.
+
+(define div-name car)
+(define div-data cadr)
+
+(define (get-record div-file employee-name)
+  (let ((proc (get 'get-record (div-name div-file) )))
+    (if proc (list (div-name div-file)(proc (div-data div-file) employee-name  )) false )))
+;employee-record is represented as a tagged data,so i can use "apply-generic". 
+(define (get-salary employee-record)
+  (apply-generic 'get-salary employee-record))
+
+(define (find-employee allFiles employee-name)
+  (if (null? allFiles) false 
+      (or (get-record (car allFiles) employee-name) (find-employee (cdr allFiles) employee-name))))
+
+;when we add new personnel divison into the system all what it wants to just do a put of the procedures it uses into the table of (operations and division-Name)
+
+
+ 	
