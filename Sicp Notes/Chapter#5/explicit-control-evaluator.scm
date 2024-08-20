462e4312f677a70a2846548d443f4246aca22e20
Assem <assem2002@gmail.com>
2024-08-20 06:56:22 +0300

    new

--- Sicp Notes/Chapter#5/explicit-control-evaluator.scm
+++ Sicp Notes/Chapter#5/explicit-control-evaluator.scm
@@ -39,6 +39,11 @@ ev-lambda
 (assign val (op make-procedure) (reg unev) (reg exp) (reg env))
 (goto (reg continue))   
 
+; It fetches the expressions and heads off to `ev-sequence`
+ev-begin
+(assign unev (op begin-actions) (reg exp))
+(save continue)
+(goto (label ev-sequence))
 
 ; ex (+ 1 2) --> We would start out be memorizing registers (continue, env) as what the next label is doing requires utilizing `continue` register.
 ; then memorizing the operands for later evaluation 
@@ -103,8 +108,109 @@ apply-dispatch
 (branch (label compound-apply))
 (goto (label unknown-procedure-type))
 primitive-apply ; It uses underlying scheme procedure `apply-primtive-procedure`, nothing fancy here (just for sake of simplicity).
-(assign val (op apply-primitive-procedure)
-(reg proc)
-(reg argl))
+(assign val (op apply-primitive-procedure) (reg proc) (reg argl))
 (restore continue)
+(goto (reg continue))
+
+; it fetches the environment of proc (I think the env is important in case of there's some insider function),
+; and fetches the parameters
+; we then extend the environemnt with the argl attached to the paramters names (as I think)
+; We then save the body of the procedure in register `unev`
+; then we head over to label `ev-sequence`
+compound-apply 
+(assign unev (op procedure-parameters) (reg proc))
+(assign env (op procedure-environment) (reg proc))
+(assign env (op extend-environment) (reg unev) (reg argl) (reg env))
+(assign unev (op procedure-body) (reg proc))
+(goto (label ev-sequence))
+
+;These 3 labels are just a while loop
+; It fetches expression by expression from the body of the proc save in register `unev`
+; We save unev and env incase some one of the expression were a procedure itself or anything that would utilize `unev` and `env`.
+; then we just eval-dispatch (it needs to be a primtive expression to us either an assignment or accessing a variable - it's eval-apply loop)
+; and we save the continue point to be label `ev-sequence-continue`.
+; then in `ev-sequence-continue` --> We just move the list of expression by one
+; When we hit the final expression we restore continue to branch out of this loop and use the continue that was meant to
+; be used when we first started evaluating the procedure application expression.
+ev-sequence
+(assign exp (op first-exp) (reg unev))
+(test (op last-exp?) (reg unev))
+(branch (label ev-sequence-last-exp))
+(save unev)
+(save env)
+(assign continue (label ev-sequence-continue))
+(goto (label eval-dispatch))
+ev-sequence-continue
+(restore env)
+(restore unev)
+(assign unev (op rest-exps) (reg unev))
+(goto (label ev-sequence))
+ev-sequence-last-exp
+(restore continue)
+(goto (label eval-dispatch))
+
+; Save the if expression and the environment and continue, They should be save for later as the predicate could
+; be for example a procedure that utilized `exp` and `env` and `continue`
+ev-if
+(save exp) ; save expression for later
+(save env)
+(save continue)
+(assign continue (label ev-if-decide))
+(assign exp (op if-predicate) (reg exp))
+(goto (label eval-dispatch)) ; evaluate the predicate
+
+; We here just restore the saved values + branch to the needed code
+; the book emphasizes on the notion of bringing back the correct environment that `if` statment should be evaluted at.
+ev-if-decide
+(restore continue)
+(restore env)
+(restore exp)
+(test (op true?) (reg val))
+(branch (label ev-if-consequent))
+ev-if-alternative
+(assign exp (op if-alternative) (reg exp))
+(goto (label eval-dispatch))
+ev-if-consequent
+(assign exp (op if-consequent) (reg exp))
+(goto (label eval-dispatch))
+
+
+; it saves the variable + env + continue
+; then goes to `eval-dispatch`, and for sure we would save in register `continue` the entry point to the conituation
+; when we return back from evaluating the expression, we use some assumed procedure set-variable-value!` to set the variable
+; in the environment with the evaluated value in `val`
+ev-assignment
+(assign unev (op assignment-variable) (reg exp))
+(save unev)
+; save variable for later
+(assign exp (op assignment-value) (reg exp))
+(save env)
+(save continue)
+(assign continue (label ev-assignment-1))
+(goto (label eval-dispatch))
+; evaluate the assignment value
+ev-assignment-1
+(restore continue)
+(restore env)
+(restore unev)
+(perform (op set-variable-value!) (reg unev) (reg val) (reg env))
+(assign val (const ok))
+(goto (reg continue))
+
+; This is similar to assignment
+; but how come this handles function defintions
+ev-definition
+(assign unev (op definition-variable) (reg exp))
+(save unev) ; save variable for later
+(assign exp (op definition-value) (reg exp))
+(save env)
+(save continue)
+(assign continue (label ev-definition-1))
+(goto (label eval-dispatch)) ; evaluate the deﬁnition value
+ev-definition-1
+(restore continue)
+(restore env)
+(restore unev)
+(perform (op define-variable!) (reg unev) (reg val) (reg env))
+(assign val (const ok))
 (goto (reg continue))
